8e666ea239aa4b56e08d1d05ea619b2fa6220da1
Xitlaly-P <jada23dem@gmail.com>
2021-01-30 23:18:41 -0500

    YOO

--- maincode.js
+++ maincode.js
@@ -1,1 +1,6 @@
-var y = 0;
+var booktitle=["Sorcery of Thorns", "Aurora Rising Illuminiae", "The Maze Runner", "Red Queen"]
+var wordcount=[]
+function change_text(){
+    var name = document.getElementById("name").value;
+    document.getElementById("demo").innerHTML = "Hello " + name;
+}
