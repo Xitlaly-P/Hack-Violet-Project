97c16bd192a42ea2bf0df33675e75b2fe3e3bd7f
Xitlaly-P <jada23dem@gmail.com>
2021-01-31 10:13:06 -0500

    text change

--- maincode.js
+++ maincode.js
@@ -1,7 +1,13 @@
-var booktitle=["Sorcery of Thorns", "Aurora Rising Illuminiae", "The Maze Runner", "Red Queen"]
+var booktitles=["Sorcery of Thorns", "Aurora Rising", "Illuminiae", "The Maze Runner", "Red Queen"]
 var wordcount=[121424, 139077, 92520, 116656]
-var cover=["2wCEAAkGBxMTEhUSExMVFRUXGBgXGRgYGBgYGxofHRgdHRoZGhgdHSggGBolHRgXIjEiJSkrLi4uFyAzODMtNygtLisBCgoKDg0OGxAQGy8mICUtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf""https://images-na.ssl-images-amazon.com/images/I/81DOo37t7UL.jpg","https://images-na.ssl-images-amazon.com/images/I/51UU0FLH1wL._SX330_BO1,204,203,200_.jpg","https://m.media-amazon.com/images/I/41LHCOh0eBL.jpg"]
+var cover=["2wCEAAkGBxMTEhUSExMVFRUXGBgXGRgYGBgYGxofHRgdHRoZGhgdHSggGBolHRgXIjEiJSkrLi4uFyAzODMtNygtLisBCgoKDg0OGxAQGy8mICUtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf", "https://images-na.ssl-images-amazon.com/images/I/81DOo37t7UL.jpg%22,%22https://images-na.ssl-images-amazon.com/images/I/51UU0FLH1wL._SX330_BO1,204,203,200_.jpg%22,%22https://m.media-amazon.com/images/I/41LHCOh0eBL.jpg"]
+var booktitle;
+var wpm;
+
 function change_text(){
-    var name = document.getElementById("name").value;
-    document.getElementById("demo").innerHTML = "Hello " + name;
+    booktitle = document.getElementById("bookInput").value;
+
+    wpm = document.getElementById("wpmInput").value;
+
+    document.getElementById("firstp").innerHTML = booktitle + ' will take you fjrei to read because of your average of ' + wpm + ' wpm';
 }
