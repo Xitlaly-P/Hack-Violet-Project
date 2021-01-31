98e689fdb86fd0bd74a9c22da36f4953d3201091
vinitaacharya <vinitaacharya2004@gmail.com>
2021-01-31 08:58:57 -0500

    morninggg

--- maincode.js
+++ maincode.js
@@ -1,5 +1,6 @@
 var booktitle=["Sorcery of Thorns", "Aurora Rising Illuminiae", "The Maze Runner", "Red Queen"]
-var wordcount=[]
+var wordcount=[121424, 139077, 92520, 116656]
+var cover=["2wCEAAkGBxMTEhUSExMVFRUXGBgXGRgYGBgYGxofHRgdHRoZGhgdHSggGBolHRgXIjEiJSkrLi4uFyAzODMtNygtLisBCgoKDg0OGxAQGy8mICUtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf""https://images-na.ssl-images-amazon.com/images/I/81DOo37t7UL.jpg","https://images-na.ssl-images-amazon.com/images/I/51UU0FLH1wL._SX330_BO1,204,203,200_.jpg","https://m.media-amazon.com/images/I/41LHCOh0eBL.jpg"]
 function change_text(){
     var name = document.getElementById("name").value;
     document.getElementById("demo").innerHTML = "Hello " + name;
